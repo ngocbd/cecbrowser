657e97677d08cc8d210b56b51f14019ef0c89541
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-08-11 18:41:52 +0700

    add SecretTemplate.java for Secret skeleton class

---
+++ src/main/java/net/cec/SecretTemplate.java
@@ -0,0 +1,12 @@
+package net.cec;
+
+public class SecretTemplate {
+	  
+	static
+	{
+		System.setProperty("webdriver.chrome.driver",
+	            "path/to/chromedriver");
+	}
+	 public static String email = "yourfacebookemail";
+	 public static String password = "yourpassword";
+}
